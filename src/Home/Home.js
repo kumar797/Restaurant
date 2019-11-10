533cd5c9e72742fb2d55ef2d780d664aed30f298
kumar797 <sravangiramoni@gmail.com>
2019-11-10 15:58:03 -0500

    Add

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -1,6 +1,10 @@
 import React, { Component } from 'react';
 import styled from 'styled-components';
 
+import Paper from '@material-ui/core/Paper';
+import image1 from '../Images/image1.jpg';
+import Fab from '@material-ui/core/Fab';
+
 const Container = styled.div`
   display: grid;
   grid-template-columns: 1fr 1fr;
@@ -17,21 +21,58 @@ const Header = styled.div`
   justify-self: center;
 `;
 
+const ImageHeader = styled.div`
+  padding: 10px;
+`;
+
+const PaperHeader = styled.div`
+  display: grid;
+  grid-gap: 8px;
+  grid-template-columns: repeat(4, 1fr);
+`;
+
 class Home extends Component {
   render() {
     const { data } = this.props;
     console.log(data);
     return (
-      <Container data-test="Home">
-        <Header>
-          <h1>{data && data.header1}</h1>
-        </Header>
+      <React.Fragment>
+        <Container data-test="Home">
+          <Header>
+            <h1>{data && data.header1}</h1>
+          </Header>
 
-        <Body>
-          <div>{data && data.header2}</div>
-          <div>{data && data.header3}</div>
-        </Body>
-      </Container>
+          <Body>
+            <div>{data && data.header2}</div>
+            <div>{data && data.header3}</div>
+          </Body>
+        </Container>
+        <div style={{ padding: '0px 10px' }}>
+          <Fab>test</Fab>
+          <PaperHeader>
+            <Paper>
+              <ImageHeader>
+                <img src={image1} alt="food" />
+              </ImageHeader>
+            </Paper>
+            <Paper>
+              <ImageHeader>
+                <img src={image1} alt="food" />
+              </ImageHeader>
+            </Paper>
+            <Paper>
+              <ImageHeader>
+                <img src={image1} alt="food" />
+              </ImageHeader>
+            </Paper>
+            <Paper>
+              <ImageHeader>
+                <img src={image1} alt="food" />
+              </ImageHeader>
+            </Paper>
+          </PaperHeader>
+        </div>
+      </React.Fragment>
     );
   }
 }
---
+++ src/Home/data.js
@@ -0,0 +1,8 @@
+const data = {
+  header1: 'OUR INTERESTING HISTORY',
+  header2:
+    'Etiam faucibus viverra libero vel efficitur. Ut semper nisl ut laoreet ultrices. Maecenas dictum arcu purus, sit amet volutpat purus viverra sit amet. Quisque lacinia quam sed tortor interdum, malesuada congue nunc ornare. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. In semper lorem eget tortor pulvinar ultricies.',
+  header3:
+    'Etiam faucibus viverra libero vel efficitur. Ut semper nisl ut laoreet ultrices. Maecenas dictum arcu purus, sit amet volutpat purus viverra sit amet. Quisque lacinia quam sed tortor interdum malesuada congue nunc ornare. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. In semper lorem eget tortor pulvinar ultricies.'
+};
+export default data;
